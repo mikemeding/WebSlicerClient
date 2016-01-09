98654be14a0c060e033983ef4041a4ed4ba6e68c
mikemeding <mikeymeding@gmail.com>
2016-01-09 16:12:44 -0500

    added angular test for uploading files to the server

--- testController.js
+++ testController.js
@@ -3,7 +3,38 @@
  */
 (function () {
     var app = angular.module("WebSlicer", []);
-    app.controller("TestController", ["$http", "$scope", function ($http, $scope) {
+    app.directive('fileModel', ['$parse', function ($parse) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                var model = $parse(attrs.fileModel);
+                var modelSetter = model.assign;
+
+                element.bind('change', function () {
+                    scope.$apply(function () {
+                        modelSetter(scope, element[0].files[0]);
+                    });
+                });
+            }
+        };
+    }]);
+
+    app.service('fileUpload', ['$http', function ($http) {
+        this.uploadFileToUrl = function (file, uploadUrl) {
+            var fd = new FormData();
+            fd.append('file', file);
+            $http.post(uploadUrl, fd, {
+                transformRequest: angular.identity,
+                headers: {'Content-Type': undefined}
+            })
+                .success(function () {
+                })
+                .error(function () {
+                });
+        }
+    }]);
+
+    app.controller("TestController", ["$http", "$scope", "fileUpload", function ($http, $scope, fileUpload) {
         $scope.title = "Web Slicer";
 
         $scope.dataHere = false;
@@ -11,10 +42,11 @@
         $scope.ping = function () {
             $http({
                 method: 'GET',
-                url: 'http://' + $scope.address + ':' + $scope.port + '/api/printer',
+                //url: 'http://' + $scope.address + ':' + $scope.port + '/api/printer',
+                url: 'http://' + $scope.address + '/api/printer',
                 headers: {
-                    'Content-Type':'application/json',
-                    'x-api-key':$scope.apiKey
+                    'Content-Type': 'application/json',
+                    'x-api-key': $scope.apiKey
                 }
             }).then(function successCallback(response) {
                 console.log(response);
@@ -23,8 +55,15 @@
             }, function errorCallback(response) {
                 console.error(response);
             });
-        }
+        };
 
+        $scope.uploadFile = function () {
+            var file = $scope.myFile;
+            console.log('file is ');
+            console.dir(file);
+            var uploadUrl = "http://localhost:8080/WebSlicer/slicer/importFile";
+            fileUpload.uploadFileToUrl(file, uploadUrl);
+        };
 
     }]);
 })();
