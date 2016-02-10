149e601d99de86a2d103d76a3b1fc74a34062f7b
mikemeding <mikeymeding@gmail.com>
2016-02-09 21:47:06 -0500

    trying to get dynamic binding

--- model/profile/settingsTabDirective.js
+++ model/profile/settingsTabDirective.js
@@ -14,56 +14,72 @@
  */
 (function () {
     var app = angular.module("WebSlicer");
-    app.controller('CuraSettingsController', ['$scope', '$http', function ($scope, $http) {
 
-        console.log($scope.content)
+    app.directive('curasettings', function ($http, $compile) {
 
-    }]);
+        var getItemTemplate = function (setting) {
+            var item = "";
+            var itemName = setting.setting;
+            var label = setting.label;
+            var type = setting.type;
+            var defaultSetting = setting.default;
 
-    app.directive('curasettings', function ($compile) {
-
-        //var getItemTemplate = function (setting) {
-        //    var item = "";
-        //    var itemName = setting.setting;
-        //    var label = setting.label;
-        //    var type = setting.type;
-        //    var defaultSetting = setting.default;
-        //
-        //    switch (type) {
-        //        case "float":
-        //            item = "<label for=\"" + itemName + "\">" + label + "</label>";
-        //            item = item + "<input value=\'" + defaultSetting + "\' type=\"number\" class=\"form-control\" id=\"" + itemName + "\">";
-        //            break;
-        //        case "int":
-        //            item = "<label for=\"" + itemName + "\">" + label + "</label>";
-        //            item = item + "<input type=\"number\" class=\"form-control\" id=\"" + itemName + "\">";
-        //            break;
-        //        case "bool":
-        //            item = "<label><input type=\"checkbox\"> " + label + "</label>";
-        //            break;
-        //        case "list":
-        //            break;
-        //    }
-        //    return "<div class=\"form-group\">" + item + "</div>";
-        //};
+            switch (type) {
+                case "float":
+                    item = "<label for=\"" + itemName + "\">" + label + "</label>";
+                    item = item + "<input value=\'" + defaultSetting + "\' type=\"number\" class=\"form-control\" id=\"" + itemName + "\">";
+                    break;
+                case "int":
+                    item = "<label for=\"" + itemName + "\">" + label + "</label>";
+                    item = item + "<input type=\"number\" class=\"form-control\" id=\"" + itemName + "\">";
+                    break;
+                case "bool":
+                    item = "<label><input type=\"checkbox\"> " + label + "</label>";
+                    break;
+                case "list":
+                    break;
+            }
+            return "<div class=\"form-group\">" + item + "</div>";
+        };
 
         var linker = function (scope, element, attrs) {
-            console.log(attrs.settings);
-            //$http.get(attrs.content).success(function (data) {
-            //    console.log(data);
-            //});
 
-            //element.html(getItemTemplate(scope.content)).show();
-            //$compile(element.contents())(scope);
+            // get the json file as specified by the directive
+            $http.get(attrs.src).success(function (data) {
+                //console.log(data);
+                scope.settings = data; // bind data to a scope that we can reference
+
+                // build html string for all setting fields
+                var htmlString = "";
+                for (var i = 0; i < scope.settings.length; i++) {
+                    htmlString += getItemTemplate(scope.settings[i]);
+                }
+                htmlString += "<button ng-click='doSomething()'>settings?</button>";
+                // bind this html to the DOM
+                element.html(htmlString).show();
+                $compile(element.contents())(scope);
+
+            });
+
+
         };
 
         return {
             restrict: 'E',
+            replace: true,
             link: linker,
             scope: {
-                settings: '='
+                src: '='
+            },
+            controller: function ($scope) {
+                $scope.doSomething = function () {
+                    console.log($scope.settings);
+                };
+                //$scope.doSomething = function (data) {
+                //    console.log("something was done");
+                //    console.log(data);
+                //};
             }
-
         };
     });
 
